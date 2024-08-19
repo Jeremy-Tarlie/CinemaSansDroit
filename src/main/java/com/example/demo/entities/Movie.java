3fe31df6a52498fdcbc05be248cb0ed8e4c7b0ee
Charles <tronel.charles@gmail.com>
2024-08-19 10:33:07 +0200

    Add Movie entity, service, repository, and controller
    Introduced the Movie entity class with JPA annotations. Created MovieService and MovieRepository to handle business logic and data persistence. Added MovieController to manage endpoints for movie-related operations.

---
+++ src/main/java/com/example/demo/entities/Movie.java
@@ -0,0 +1,19 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "movie")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_movie;
+}
